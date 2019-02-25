4b559f677a01be02ac0c504f22bfae32730a6215
HieuNguyen202 <edwardnguyen202@gmail.com>
2019-02-25 09:57:51 -0600

    added client and server folders

---
+++ client/src/main.cpp
@@ -0,0 +1,34 @@
+#include <Arduino.h>
+#include <WiFi.h>
+int count  = 0;
+const char* ssid = "Mars";
+const char* password = "3941HIEU";
+IPAddress server(192,168,0,21);
+WiFiClient client;
+void setup() {
+  Serial.begin(9600);
+  WiFi.begin(ssid, password);
+  while(WiFi.status()!=WL_CONNECTED){
+    delay(500);
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+  }
+  Serial.println("Connected to Mars!");
+  Serial.print("Your local IP address is ");
+  Serial.println(WiFi.localIP());
+
+  if(client.connect(server,1234)){
+    Serial.println("Connected to server!");
+    client.println("GET /search?q=arduino HTTP/1.0");
+    client.println();
+  } else {
+    Serial.println("Failed to connect to server!");
+  }
+  
+}
+
+void loop() {
+  // Serial.println(count);
+  // count++;
+  // put your main code here, to run repeatedly:
+}
