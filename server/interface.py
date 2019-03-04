fbb6392120960fe56d0887146df7d133e4d61537
Hieu <edwardnguyen202@gmail.com>
2019-03-04 17:58:27 -0600

    Implement Node class. Make the tcpclient an attachment of the the Node class

--- server/interface.py
+++ server/interface.py
@@ -1,6 +1,9 @@
 from PyQt5.QtNetwork import QHostAddress, QTcpServer, QTcpSocket, QNetworkInterface, QAbstractSocket
 from PyQt5.QtCore import pyqtSignal, QObject
 MESSAGE_LENGTH = 5  #Including checksum
+LIGHT = 'l'
+SHADE_POS = 'p'
+SET_SHADE_POS = 'p'
 
 def getIPAddress():
     for ipAddress in QNetworkInterface.allAddresses():
@@ -28,11 +31,70 @@ class Server(QTcpServer):
     def acceptErrorHandler(self, socketError):
         print("interface.py: Server accept error", socketError)
 
-class GUI(QObject):
-    pass
-
-class CLI(QObject):
-    pass
-
 class Node(QObject):
-    pass
+    newOutput = pyqtSignal(bytes)
+
+    def __init__(self, tcpClient):
+        super(Monitor, self).__init__()
+        self.tcpClient = None
+        self.attach(tcpClient)
+
+    def attach(self, tcpClient):
+        if self.tcpClient != None:
+            self.detach()
+        tcpClient.readyRead.connect(self.newInput)
+        tcpClient.error.connect(self.connectionError)
+        self.newOutput.connect(tcpClient.write)
+        self.tcpClient = tcpClient
+        self.tcpClientAttached()
+
+    #Detach the tcpClient from the Node
+    def detach(self):
+        self.tcpClient.readyRead.disconnect(self.newInput)
+        self.tcpClient.error.disconnect(self.connectionError)
+        self.newOutput.disconnect(tcpClient.write)
+        self.tcpClient = None
+        self.tcpClientDetached()
+
+    def write(self, command, val):
+        message = command + str(val).zfill(MESSAGE_LENGTH-1)
+        self.tcpClient.write(message)
+        #TODO send the message
+
+
+    def newInput(self, input):
+        if input[0] == LIGHT:
+            self.newSensorData(int(input[1:]))
+        elif input[0] == SHADE_POS:
+            self.newShadePos(int(input[1:]))
+        else:
+            print('Unknown input:', input)
+
+    def setSharePos(self, val):
+
+        print("Need to implement newSensorData")
+
+    def newSensorData(self, val):
+        print("Need to implement newSensorData")
+
+    def newShadePos(self, val):
+        print("Need to implement newShadePos")
+
+    def tcpClientAttached(self):
+        print("Need to implement tcpClientAttached")
+
+    def tcpClientDetached(self):
+        print("Need to implement tcpClientDetached")
+
+    def connectionError(self):
+        print("Need to implement connectionError")
+
+class CLINode(Node):
+    def __init__(self):
+        super(CLI, self).__init__()
+
+    def newSensorData(self, val):
+        pass
+
+    def newShadePos(self, val):
+        pass
